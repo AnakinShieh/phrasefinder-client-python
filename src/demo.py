7c8dd6ffb31e596733559ebd95f2d958cf2fdb90
Martin Trenkmann <martin.trenkmann@posteo.de>
2016-09-25 11:29:05 +0200

    Updated formatting

--- src/demo.py
+++ src/demo.py
@@ -3,21 +3,21 @@
 import phrasefinder
 
 def main():
-
+    
     # Set up your query.
     query = 'I like ???'
-
+    
     # Set the optional parameter topk to 10.
     options = phrasefinder.Options()
     options.topk = 10
-
+    
     # Perform a request.
     try:
         result = phrasefinder.search(query, options)
         if result.status != phrasefinder.Status.Ok:
             print 'Request was not successful: %d' % result.status
             return
-
+        
         # Print phrases line by line.
         for phrase in result.phrases:
             print '%6f' % phrase.score,
@@ -25,7 +25,7 @@ def main():
                 print '%s_%d' % (token.text, token.tag),
             print
         print 'Remaining quota: %d' % result.quota
-
+    
     except Exception, e:
         print 'Some error occurred: %s' % e
 
